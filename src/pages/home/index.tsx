7a6f66258d4c91d9a955a142804b9d2e1000547d
Elias Forte <eliasforte.dev@gmail.com>
2022-07-20 21:11:10 -0300

    feat(pages): add `Card` into page `Home`

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,24 +1,74 @@
+/* eslint-disable no-unused-vars */
 import * as React from 'react';
+import {
+  Box,
+  Flex,
+  Grid,
+  GridItem,
+  Text,
+} from '@chakra-ui/react';
 import { PokemonContext } from '../../context/pokemonContext';
 import Sidebar from '../../components/sidebar';
 import DrawerMenu from '../../components/drawer';
+import Search from '../../components/search';
+import Card from '../../components/card';
 
 const Home: React.FC = () => {
   const { pokemonList } = React.useContext(PokemonContext);
+  const newID = React.useId();
 
   return (
-    <div>
-      <Sidebar />
-      <DrawerMenu />
-      {
-        pokemonList.results.map((item) => (
-          <>
-            <p>{item.name}</p>
-            <img src={item.image} alt={item.name} />
-          </>
-        ))
-      }
-    </div>
+    <Flex bg="gray.100" h="100vh" flexDirection="column">
+      <Box>
+        <Sidebar />
+        <DrawerMenu />
+      </Box>
+      <Grid
+        templateColumns={['1fr', '1fr', '1fr', '1fr', '1fr']}
+        gridTemplateColumns={
+          [
+            '1fr',
+            '1fr',
+            'repeat(2, 1fr)',
+            'repeat(3, 1fr)',
+            'repeat(3, 1fr)',
+          ]
+        }
+        gridTemplateRows="repeat(3, 1fr)"
+        justifyItems="center"
+        maxW={['100%', '100%', '100%']}
+        gridGap="16px"
+        m={
+          [
+            '30px 50px',
+            '30px 100px',
+            '50px 30px',
+            '0 80px',
+            '25px 200px',
+          ]
+        }
+      >
+        <GridItem
+          colSpan={[1, 1, 2, 3, 3]}
+          w={['100%', '100%', '100%', '100%', '100%']}
+          margin="auto auto"
+        >
+          <Search />
+        </GridItem>
+        {
+      pokemonList.results.map((pokemon, index) => (
+        <GridItem
+          key={`${newID}${pokemon.name}`}
+          justifyItems="center"
+          w={['100%', '100%', '100%', '100%', '100%']}
+        >
+          <Card pokemon={pokemon} index={index} />
+        </GridItem>
+      ))
+    }
+      </Grid>
+    </Flex>
+
   );
 };
 
