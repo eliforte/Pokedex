58476f08c1360178718c97e434bf9973c776ad6a
Elias Forte <eliasforte.dev@gmail.com>
2022-07-22 11:27:48 -0300

    style(components): centralizing components

--- src/components/emptyList/index.tsx
+++ src/components/emptyList/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Box,
+  Flex,
   Image,
   Heading,
   Text,
@@ -12,27 +12,32 @@ const EmptyList: React.FC = () => {
   const { pathname } = useLocation();
 
   return (
-    <Box>
+    <Flex
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
       <Image
         src="emptyfolder.svg"
         alt="nenhum pokemon encontrado"
-        h={['100px', '100px', '115px', '130px', '150px']}
-        w={['100px', '100px', '115px', '130px', '150px']}
+        h={['100px', '100px', '115px', '130px', '130px']}
+        w={['100px', '100px', '115px', '130px', '130px']}
+        mb="20px"
       />
-      <Heading as="h3" size="lg">
+      <Heading mb="10px" as="h3" size={['md', 'md', 'md', 'md', 'md']}>
         Nenhum pokemon encontrado
       </Heading>
       {
         pathname === '/pokedex'
           ? (
-            <Text>
+            <Text color="gray.400" mb="20px">
               Capture pokemons,
-              <Link fontStyle="bold" as={ReactLink} to="/" color="blue.400"> clique aqui.</Link>
+              <Link fontWeight="600" as={ReactLink} to="/" color="#3182CE"> clique aqui.</Link>
             </Text>
           )
-          : <Text>Verifique se digitou corretamente</Text>
+          : <Text mb="20px">Verifique se digitou corretamente</Text>
       }
-    </Box>
+    </Flex>
   );
 };
 
