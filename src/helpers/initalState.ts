bf2a041286d3fe8b8bd70cbaff88db6379a3f0c9
Elias Forte <eliasforte.dev@gmail.com>
2022-07-23 20:44:30 -0300

    feat(helpers): created file `initiaState`

---
+++ src/helpers/initalState.ts
@@ -0,0 +1,41 @@
+const InitialState = {
+  pokemonList: {
+    count: 0,
+    next: '',
+    previous: '',
+    results: [{
+      name: '',
+      image: '',
+      type: '',
+      id: 0,
+    }],
+  },
+  setPokemonList: () => {},
+  pokemon: {
+    id: 0,
+    image: '',
+    name: '',
+    description: '',
+    type: '',
+    height: 0,
+    weight: 0,
+    abilities: '',
+    stats: [{
+      statName: '',
+      base_stat: 0,
+    }],
+    total: 0,
+    evolutions: [],
+  },
+  setPokemon: () => {},
+  isLoading: false,
+  setIsLoading: () => {},
+  error: '',
+  setError: () => {},
+  getPokemonDetails: () => {},
+  search: '',
+  setSearch: () => {},
+  firstRenderPokemons: () => {},
+};
+
+export default InitialState;
